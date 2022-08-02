7ed82223c3f2031dc10c44d07c533c4469996f5d
Sergio Córdoba <scordobamuriel@gmail.com>
2022-08-02 09:38:18 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    #return HttpResponse('<h1>Welcome to Home Page</h1>')
+    return render(request, 'home.html', {'name':'Sergio Córdoba'})
